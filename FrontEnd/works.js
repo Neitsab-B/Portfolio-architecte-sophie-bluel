96f17a0cbb8b79e46d2044694f9a09bf35e60975
Neitsab-B <bbarbe.pro@gmail.com>
2023-01-31 11:10:31 +0100

    update of filters
    -filters now work with an API call of categories and buttons are created with API call too
    -minor updates of HTML(erase old buttons) and CSS

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,20 +1,8 @@
-// test de retour de catégorie par get categories
-
-// coucou github
-
-async function getCategoriesWorks() {
-  const r = await fetch("http://localhost:5678/api/categories")
-  if (r.ok === true) {
-    return r.json()
-  }
-}
-
-getCategoriesWorks()
-
-// old work
-
+const filtersContainer = document.querySelector("#filters");
 const divGallery = document.querySelector(".gallery");
+const btnAll = document.querySelector("#btn-all");
 
+// fonction pour appeller l'API des projets
 async function getDataWorks() {
   const r = await fetch("http://localhost:5678/api/works");
   if (r.ok === true) {
@@ -23,8 +11,10 @@ async function getDataWorks() {
   throw new Error("Impossible de contacter le serveur");
 }
 
+// fonction pour créer tous les projets
 function createWorks() {
   getDataWorks().then((dataWorks) => {
+    document.querySelector(".gallery").innerHTML = "";
     dataWorks.forEach((work) => {
       const figureWork = document.createElement("figure");
       const imageWork = document.createElement("img");
@@ -36,6 +26,8 @@ function createWorks() {
       figureWork.appendChild(imageWork);
       figureWork.appendChild(titleWork);
 
+      //attribution des titres, images, alt, taille etc
+
       imageWork.src = work.imageUrl;
       imageWork.crossOrigin = "anonymous";
       imageWork.style.width = "310px";
@@ -46,108 +38,114 @@ function createWorks() {
   });
 }
 
+// création des projets au loading de la page
 createWorks();
 
-// BOUTONS DE FILTRES
+// fonction pour appeller l'API des catégories
+async function getCategories() {
+  const r = await fetch("http://localhost:5678/api/categories");
+  if (r.ok === true) {
+    return r.json();
+  }
+}
 
-const btnAll = document.querySelector(".filterAll");
+// appel de l'API catégories pour créer les bouttons
+getCategories().then((dataCategories) => {
+  dataCategories.forEach((categories) => {
+    const categoriesButtons = document.createElement("button");
+    categoriesButtons.textContent = categories.name;
+    categoriesButtons.name = categories.name;
+
+    filtersContainer.appendChild(categoriesButtons);
+
+    // filtres clickables selon l'id de la catégorie
+    categoriesButtons.addEventListener("click", function () {
+      if (categories.id === 1) {
+        getDataWorks().then((dataWorks) => {
+          const filteredWork = dataWorks.filter(
+            (work) => work.categoryId === 1
+          );
+
+          //effacement de la page avant le filtre
+
+          document.querySelector(".gallery").innerHTML = "";
+
+          filteredWork.forEach((work) => {
+            const figureWork = document.createElement("figure");
+            const imageWork = document.createElement("img");
+            const titleWork = document.createElement("figcaption");
+
+            // attribution des parents
+
+            divGallery.appendChild(figureWork);
+            figureWork.appendChild(imageWork);
+            figureWork.appendChild(titleWork);
+
+            //attribution des titres, images, alt, taille etc
+
+            imageWork.crossOrigin = "anonymous";
+            imageWork.src = work.imageUrl;
+            imageWork.style.width = "310px";
+            imageWork.style.height = "413px";
+            imageWork.alt = work.title;
+            titleWork.innerText = work.title;
+          });
+        });
+      }
+      if (categories.id === 2) {
+        getDataWorks().then((dataWorks) => {
+          const filteredWork = dataWorks.filter(
+            (work) => work.categoryId === 2
+          );
+          document.querySelector(".gallery").innerHTML = "";
+          filteredWork.forEach((work) => {
+            const figureWork = document.createElement("figure");
+            const imageWork = document.createElement("img");
+            const titleWork = document.createElement("figcaption");
+
+            divGallery.appendChild(figureWork);
+            figureWork.appendChild(imageWork);
+            figureWork.appendChild(titleWork);
+
+            imageWork.src = work.imageUrl;
+            imageWork.crossOrigin = "anonymous";
+            imageWork.style.width = "310px";
+            imageWork.style.height = "413px";
+            imageWork.alt = work.title;
+            titleWork.innerText = work.title;
+          });
+        });
+      }
+      if (categories.id === 3) {
+        getDataWorks().then((dataWorks) => {
+          const filteredWork = dataWorks.filter(
+            (work) => work.categoryId === 3
+          );
+          document.querySelector(".gallery").innerHTML = "";
+          filteredWork.forEach((work) => {
+            const figureWork = document.createElement("figure");
+            const imageWork = document.createElement("img");
+            const titleWork = document.createElement("figcaption");
+
+            divGallery.appendChild(figureWork);
+            figureWork.appendChild(imageWork);
+            figureWork.appendChild(titleWork);
+
+            imageWork.src = work.imageUrl;
+            imageWork.crossOrigin = "anonymous";
+            imageWork.style.width = "310px";
+            imageWork.style.height = "413px";
+            imageWork.alt = work.title;
+            titleWork.innerText = work.title;
+          });
+        });
+      }
+    });
+  });
+});
+
+// filtre "tous"
 
 btnAll.addEventListener("click", function () {
-  document.querySelector(".gallery").innerHTML = "";
   createWorks();
-});
-
-// filtre objets
-
-const btnFilterObjects = document.querySelector(".filterObjects");
-
-btnFilterObjects.addEventListener("click", function () {
-  getDataWorks().then((dataWorks) => {
-    const filteredObjects = dataWorks.filter(
-      (objects) => objects.categoryId === 1
-    );
-    document.querySelector(".gallery").innerHTML = "";
-    filteredObjects.forEach((objects) => {
-      const figureWork = document.createElement("figure");
-      const imageWork = document.createElement("img");
-      const titleWork = document.createElement("figcaption");
-
-      // attribution des parents
-
-      divGallery.appendChild(figureWork);
-      figureWork.appendChild(imageWork);
-      figureWork.appendChild(titleWork);
-
-      imageWork.src = objects.imageUrl;
-      imageWork.crossOrigin = "anonymous";
-      imageWork.style.width = "310px";
-      imageWork.style.height = "413px";
-      imageWork.alt = objects.title;
-      titleWork.innerText = objects.title;
-    });
-  });
-});
-
-// filtre appartements
-
-const btnFilterLogements = document.querySelector(".filterLogements");
-
-btnFilterLogements.addEventListener("click", function () {
-  getDataWorks().then((dataWorks) => {
-    const filteredLogements = dataWorks.filter(
-      (logements) => logements.categoryId === 2
-    );
-    document.querySelector(".gallery").innerHTML = "";
-    filteredLogements.forEach((logements) => {
-      const figureWork = document.createElement("figure");
-      const imageWork = document.createElement("img");
-      const titleWork = document.createElement("figcaption");
-
-      // attribution des parents
-
-      divGallery.appendChild(figureWork);
-      figureWork.appendChild(imageWork);
-      figureWork.appendChild(titleWork);
-
-      imageWork.src = logements.imageUrl;
-      imageWork.crossOrigin = "anonymous";
-      imageWork.style.width = "310px";
-      imageWork.style.height = "413px";
-      imageWork.alt = logements.title;
-      titleWork.innerText = logements.title;
-    });
-  });
-});
-
-// filtre hôtels et restaurants
-
-const btnFilterHotelsAndRestaurants = document.querySelector(
-  ".filterHotelsAndRestaurants"
-);
-
-btnFilterHotelsAndRestaurants.addEventListener("click", function () {
-  getDataWorks().then((dataWorks) => {
-    const filteredHotelsAndRestaurants = dataWorks.filter(
-      (hotelsAndRestaurants) => hotelsAndRestaurants.categoryId === 3
-    );
-    document.querySelector(".gallery").innerHTML = "";
-    filteredHotelsAndRestaurants.forEach((hotelsAndRestaurants) => {
-      const figureWork = document.createElement("figure");
-      const imageWork = document.createElement("img");
-      const titleWork = document.createElement("figcaption");
-
-      // attribution des parents
-
-      divGallery.appendChild(figureWork);
-      figureWork.appendChild(imageWork);
-      figureWork.appendChild(titleWork);
-
-      imageWork.src = hotelsAndRestaurants.imageUrl;
-      imageWork.crossOrigin = "anonymous";
-      imageWork.style.width = "310px";
-      imageWork.style.height = "413px";
-      imageWork.alt = hotelsAndRestaurants.title;
-      titleWork.innerText = hotelsAndRestaurants.title;
-    });
-  });
 });
